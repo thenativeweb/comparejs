c07d76e85c7a5d77c7f2bb2caa1a1e4e5aeebf5d
Golo Roden <webmaster@goloroden.de>
2012-08-18 09:41:04 +0200

    Added operators for undefined.

---
+++ test/undefined.js
@@ -0,0 +1,40 @@
+var cmp = require('../lib/index'),
+    should = require('should');
+
+suite('undefined x undefined', function () {
+  suite('eq', function () {
+    suite('equal => true', function () {
+      test('undefined x undefined', function () { cmp.eq(undefined, undefined).should.equal(true); });
+    });
+  });
+
+  suite('ne', function () {
+    suite('equal => false', function () {
+      test('undefined x undefined', function () { cmp.ne(undefined, undefined).should.equal(false); });
+    });
+  });
+
+  suite('gt', function () {
+    suite('equal => false', function () {
+      test('undefined x undefined', function () { cmp.gt(undefined, undefined).should.equal(false); });
+    });
+  });
+
+  suite('ge', function () {
+    suite('equal => true', function () {
+      test('undefined x undefined', function () { cmp.ge(undefined, undefined).should.equal(true); });
+    });
+  });
+
+  suite('lt', function () {
+    suite('equal => false', function () {
+      test('undefined x undefined', function () { cmp.lt(undefined, undefined).should.equal(false); });
+    });
+  });
+
+  suite('le', function () {
+    suite('equal => true', function () {
+      test('undefined x undefined', function () { cmp.le(undefined, undefined).should.equal(true); });
+    });
+  });
+});
