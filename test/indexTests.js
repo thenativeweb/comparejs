0a2d4f211941d1f527fd40f22e02268531ba7142
Golo Roden <webmaster@goloroden.de>
2012-08-18 06:53:04 +0200

    Implemented ne for numbers.

--- test/indexTests.js
+++ test/indexTests.js
@@ -38,7 +38,38 @@ suite('compare.js', function () {
   });
 
   suite('ne', function () {
+    suite('number x number', function () {
+      suite('equal => true', function () {
+        test('int x int', function () {
+          cmp.ne(23, 23).should.equal(false);
+        });
 
+        test('double x double', function () {
+          cmp.ne(3.14159, 3.14159).should.equal(false);
+        });
+      });
+      suite('not equal => false', function () {
+        test('int x int', function () {
+          cmp.ne(23, 42).should.equal(true);
+        });
+
+        test('double x double', function () {
+          cmp.ne(3.14159, 2.71828).should.equal(true);
+        });
+
+        test('int x isNaN', function () {
+          cmp.ne(23, Number.NaN).should.equal(true);
+        });
+
+        test('isNaN x isNaN', function () {
+          cmp.ne(Number.NaN, Number.NaN).should.equal(true);
+        });
+
+        test('isNan x int', function () {
+          cmp.ne(Number.NaN, 42).should.equal(true);
+        });
+      });
+    });
   });
 
   suite('gt', function () {
