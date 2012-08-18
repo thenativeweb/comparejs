2a005c89f8941ead93a8ad3f76fdda5b7d279e8b
Golo Roden <webmaster@goloroden.de>
2012-08-18 07:14:55 +0200

    Streamlined suites.

--- test/numberXnumber.js
+++ test/numberXnumber.js
@@ -1,139 +1,135 @@
 var cmp = require('../lib/index'),
     should = require('should');
 
-suite('compare.js', function () {
+suite('number x number', function () {
   suite('eq', function () {
-    suite('number x number', function () {
-      suite('equal => true', function () {
-        test('int x int', function () {
-          cmp.eq(23, 23).should.equal(true);
-        });
-
-        test('int x Int()', function () {
-          cmp.eq(23, new Number(23)).should.equal(true);
-        });
-
-        test('Int() x Int()', function () {
-          cmp.eq(new Number(23), new Number(23)).should.equal(true);
-        });
-
-        test('double x double', function () {
-          cmp.eq(3.14159, 3.14159).should.equal(true);
-        });
-
-        test('double x Double()', function () {
-          cmp.eq(3.14159, new Number(3.14159)).should.equal(true);
-        });
-
-        test('Double() x Double()', function () {
-          cmp.eq(new Number(3.14159), new Number(3.14159)).should.equal(true);
-        });
+    suite('equal => true', function () {
+      test('int x int', function () {
+        cmp.eq(23, 23).should.equal(true);
       });
 
-      suite('not equal => false', function () {
-        test('int x int', function () {
-          cmp.eq(23, 42).should.equal(false);
-        });
+      test('int x Int()', function () {
+        cmp.eq(23, new Number(23)).should.equal(true);
+      });
 
-        test('int x Int()', function () {
-          cmp.eq(23, new Number(42)).should.equal(false);
-        });
+      test('Int() x Int()', function () {
+        cmp.eq(new Number(23), new Number(23)).should.equal(true);
+      });
 
-        test('Int() x Int()', function () {
-          cmp.eq(new Number(23), new Number(42)).should.equal(false);
-        });
+      test('double x double', function () {
+        cmp.eq(3.14159, 3.14159).should.equal(true);
+      });
 
-        test('double x double', function () {
-          cmp.eq(3.14159, 2.71828).should.equal(false);
-        });
+      test('double x Double()', function () {
+        cmp.eq(3.14159, new Number(3.14159)).should.equal(true);
+      });
 
-        test('double x Double()', function () {
-          cmp.eq(3.14159, new Number(2.71828)).should.equal(false);
-        });
+      test('Double() x Double()', function () {
+        cmp.eq(new Number(3.14159), new Number(3.14159)).should.equal(true);
+      });
+    });
 
-        test('Double() x Double()', function () {
-          cmp.eq(new Number(3.14159), new Number(2.71828)).should.equal(false);
-        });
+    suite('not equal => false', function () {
+      test('int x int', function () {
+        cmp.eq(23, 42).should.equal(false);
+      });
 
-        test('int x isNaN', function () {
-          cmp.eq(23, Number.NaN).should.equal(false);
-        });
+      test('int x Int()', function () {
+        cmp.eq(23, new Number(42)).should.equal(false);
+      });
 
-        test('isNaN x isNaN', function () {
-          cmp.eq(Number.NaN, Number.NaN).should.equal(false);
-        });
+      test('Int() x Int()', function () {
+        cmp.eq(new Number(23), new Number(42)).should.equal(false);
+      });
 
-        test('isNan x int', function () {
-          cmp.eq(Number.NaN, 42).should.equal(false);
-        });
+      test('double x double', function () {
+        cmp.eq(3.14159, 2.71828).should.equal(false);
+      });
+
+      test('double x Double()', function () {
+        cmp.eq(3.14159, new Number(2.71828)).should.equal(false);
+      });
+
+      test('Double() x Double()', function () {
+        cmp.eq(new Number(3.14159), new Number(2.71828)).should.equal(false);
+      });
+
+      test('int x isNaN', function () {
+        cmp.eq(23, Number.NaN).should.equal(false);
+      });
+
+      test('isNaN x isNaN', function () {
+        cmp.eq(Number.NaN, Number.NaN).should.equal(false);
+      });
+
+      test('isNan x int', function () {
+        cmp.eq(Number.NaN, 42).should.equal(false);
       });
     });
   });
 
   suite('ne', function () {
-    suite('number x number', function () {
-      suite('equal => false', function () {
-        test('int x int', function () {
-          cmp.ne(23, 23).should.equal(false);
-        });
-
-        test('int x Int()', function () {
-          cmp.ne(23, new Number(23)).should.equal(false);
-        });
-
-        test('Int() x Int()', function () {
-          cmp.ne(new Number(23), new Number(23)).should.equal(false);
-        });
-
-        test('double x double', function () {
-          cmp.ne(3.14159, 3.14159).should.equal(false);
-        });
-
-        test('double x Double()', function () {
-          cmp.ne(3.14159, new Number(3.14159)).should.equal(false);
-        });
-
-        test('Double() x Double()', function () {
-          cmp.ne(new Number(3.14159), new Number(3.14159)).should.equal(false);
-        });
+    suite('equal => false', function () {
+      test('int x int', function () {
+        cmp.ne(23, 23).should.equal(false);
       });
-      
-      suite('not equal => true', function () {
-        test('int x int', function () {
-          cmp.ne(23, 42).should.equal(true);
-        });
 
-        test('int x Int()', function () {
-          cmp.ne(23, new Number(42)).should.equal(true);
-        });
+      test('int x Int()', function () {
+        cmp.ne(23, new Number(23)).should.equal(false);
+      });
 
-        test('Int() x Int()', function () {
-          cmp.ne(new Number(23), new Number(42)).should.equal(true);
-        });
+      test('Int() x Int()', function () {
+        cmp.ne(new Number(23), new Number(23)).should.equal(false);
+      });
 
-        test('double x double', function () {
-          cmp.ne(3.14159, 2.71828).should.equal(true);
-        });
+      test('double x double', function () {
+        cmp.ne(3.14159, 3.14159).should.equal(false);
+      });
 
-        test('double x Double()', function () {
-          cmp.ne(3.14159, new Number(2.71828)).should.equal(true);
-        });
+      test('double x Double()', function () {
+        cmp.ne(3.14159, new Number(3.14159)).should.equal(false);
+      });
 
-        test('Double() x Double()', function () {
-          cmp.ne(new Number(3.14159), new Number(2.71828)).should.equal(true);
-        });
+      test('Double() x Double()', function () {
+        cmp.ne(new Number(3.14159), new Number(3.14159)).should.equal(false);
+      });
+    });
+    
+    suite('not equal => true', function () {
+      test('int x int', function () {
+        cmp.ne(23, 42).should.equal(true);
+      });
 
-        test('int x isNaN', function () {
-          cmp.ne(23, Number.NaN).should.equal(true);
-        });
+      test('int x Int()', function () {
+        cmp.ne(23, new Number(42)).should.equal(true);
+      });
 
-        test('isNaN x isNaN', function () {
-          cmp.ne(Number.NaN, Number.NaN).should.equal(true);
-        });
+      test('Int() x Int()', function () {
+        cmp.ne(new Number(23), new Number(42)).should.equal(true);
+      });
 
-        test('isNan x int', function () {
-          cmp.ne(Number.NaN, 42).should.equal(true);
-        });
+      test('double x double', function () {
+        cmp.ne(3.14159, 2.71828).should.equal(true);
+      });
+
+      test('double x Double()', function () {
+        cmp.ne(3.14159, new Number(2.71828)).should.equal(true);
+      });
+
+      test('Double() x Double()', function () {
+        cmp.ne(new Number(3.14159), new Number(2.71828)).should.equal(true);
+      });
+
+      test('int x isNaN', function () {
+        cmp.ne(23, Number.NaN).should.equal(true);
+      });
+
+      test('isNaN x isNaN', function () {
+        cmp.ne(Number.NaN, Number.NaN).should.equal(true);
+      });
+
+      test('isNan x int', function () {
+        cmp.ne(Number.NaN, 42).should.equal(true);
       });
     });
   });
